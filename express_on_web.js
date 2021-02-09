0e88b1fde880d59036330a6f42ffe4fb39f64e0d
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2021-02-09 12:38:02 +0530

    Add files via upload

---
+++ express_on_web.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.get('/',(req,res)=>{
+    res.send("Hello Express js");
+})
+app.listen(3000,()=>{console.log("Express is running");})
