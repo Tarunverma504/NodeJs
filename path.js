d2c0c95784eccaa3bf16065a113ead32de3b43f6
Tarun_Verma <59416189+Tarunverma504@users.noreply.github.com>
2021-02-21 10:52:00 +0530

    Add files via upload

---
+++ path.js
@@ -0,0 +1,5 @@
+const path= require('path');
+console.log("Path dir name:- "+path.dirname('E:\nodejs\path.js'));
+console.log("path Extension:- "+path.extname('E:\nodejs\path.js'));
+console.log("path basename:- "+path.basename('E:\nodejs\path.js'));
+console.log("path parse:- "+path.parse('E:\nodejs\path.js'));
